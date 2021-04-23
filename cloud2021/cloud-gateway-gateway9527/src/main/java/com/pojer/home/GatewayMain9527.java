69bb8f09a54d7bab8319a473452d1a1caf102522
pojer <pangjie1972@163.com>
2021-04-24 07:25:23 +0800

    1、gateway异步非阻塞模型

---
+++ cloud2021/cloud-gateway-gateway9527/src/main/java/com/pojer/home/GatewayMain9527.java
@@ -0,0 +1,20 @@
+package com.pojer.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: pojer
+ * @Date: 2021/04/23/下午 8:33
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class);
+    }
+}
